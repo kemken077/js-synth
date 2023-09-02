b1ab79c55b3f79b2db7dbe8aff92073d8fc1fa80
¨kemken077¨ <kemken077@gmail.com>
2023-09-02 10:40:56 -0300

    Init waveforms

--- static/scripts/app.js
+++ static/scripts/app.js
@@ -3,15 +3,19 @@ import {
   getNewAudioContext,
   connectMasterVolume,
   mapVolumeControls,
+  setWaveforms,
  } from './modules/audio.js';
 
 function main() {
   const context = getNewAudioContext();
   const masterVolume = connectMasterVolume(context);
+  const waveforms = setWaveforms();
 
-  mapVolumeControls(masterVolume);
-
-  log({context, masterVolume});
+  log({
+    context,
+    masterVolume,
+    waveforms,
+  });
 }
 
 window.addEventListener('DOMContentLoaded', () => {
